b780cb506f34c3295c1bf448e0b906afbd721554
ebenKB <ekb.adjei@yahoo.com>
2021-07-10 17:37:45 +0000

    Remove horizontal scroll list from places

--- src/pages/Places/PlaceDetails.js
+++ src/pages/Places/PlaceDetails.js
@@ -28,9 +28,9 @@ const PlaceDetails = (props) => {
           <div className="bg-gray-200 mb-5">
             <Map  location={place.acf.map }/>
           </div>
-          <div className="mt-5 mb-5">
+          {/* <div className="mt-5 mb-5">
             <HScrollList />
-          </div>
+          </div> */}
           <div className="container pt-5">
             <p>{place.acf.content}</p>
           </div>
